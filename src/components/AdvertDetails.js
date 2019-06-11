e28c22b56fc8234fb898a1e6bbe21b01c72fb3d3
KevinZijlmansgit config --global user.email kevin.zijlmans22@gmail.com <kevin.zijlmans22@gmail.com>
2019-06-11 12:30:00 +0200

    Added home button to details page

--- src/components/AdvertDetails.js
+++ src/components/AdvertDetails.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
 
 export default function AdvertDetails(props) {
     return (
+        <div>
         <div>
             <h1>{props.advert.title}</h1>       
             <p>{props.advert.description}</p>
@@ -9,6 +12,12 @@ export default function AdvertDetails(props) {
             <p>{props.advert.phoneNumber}</p>
             <img src={props.advert.picture} alt="Productpics" height="42" width="42"/>
             <p>{props.advert.price}</p>
+        </div>
+        <div>
+            <Button variant="contained" color="primary">
+                Home
+            </Button>
+        </div>
         </div>
     )
 }
