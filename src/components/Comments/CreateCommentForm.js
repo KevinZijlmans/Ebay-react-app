c4d411689e767aa808a1ace1acb14e52e3873299
KevinZijlmansgit config --global user.email kevin.zijlmans22@gmail.com <kevin.zijlmans22@gmail.com>
2019-06-18 16:50:38 +0200

    Added CreateCommentForm component

---
+++ src/components/Comments/CreateCommentForm.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {createComment} from '../actions/comments'
+import CommentForm from './CommentForm'
+
+class CreateCommentFormContainer extends React.Component {
+  state = {
+    text: '',
+    advertId: this.props.match.params.id,
+  }
+  onChange = (comments) => {
+    this.setState({
+      [comments.target.name]: comments.target.value
+    })
+  }
+
+  onSubmit = (comments) => {
+    comments.preventDefault()
+    this.setState({
+      text: ''
+    })
+    this.props.createComment(this.state)
+  }
+
+  render() {
+    return (<CommentForm
+      onSubmit={this.onSubmit}
+      onChange={this.onChange}
+      values={this.state}
+    />)
+  }
+}
+const mapStateToProps = state => ({
+  advert: state.advert,
+  comments: state.comments
+})
+
+export default connect(mapStateToProps, {createComment})(CreateCommentFormContainer)
