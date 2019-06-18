dfa96621b44cbbf3bea36aa1365536640efbd68d
KevinZijlmansgit config --global user.email kevin.zijlmans22@gmail.com <kevin.zijlmans22@gmail.com>
2019-06-18 16:56:01 +0200

    Added comment actions

---
+++ src/actions/comments.js
@@ -0,0 +1,37 @@
+import request from 'superagent'
+
+export const COMMENTS_FETCHED = 'COMMENTS_FETCHED'
+export const COMMENT_CREATE_SUCCESS = 'COMMENT_CREATE_SUCCESS'
+
+const baseUrl = 'http://localhost:4000'
+
+const commentsFetched = comments => ({
+    type: COMMENTS_FETCHED,
+    comments
+  })
+  
+  export const loadComments = () => (dispatch, getState) => {
+  
+    if (getState().comments.length) 
+      return
+      request(`${baseUrl}/comments`)
+        .then(response => {
+          dispatch(commentsFetched(response.body.comments))
+        })
+          .catch(console.error)
+  }
+  
+  const commentCreateSuccess = comment => ({
+    type: COMMENT_CREATE_SUCCESS,
+    comment
+  })
+  
+  export const createComment = (data, id) => (dispatch) => { 
+    request
+      .post(`${baseUrl}/adverts/${id}/comments`)
+      .send(data)
+      .then(response => {
+        dispatch(commentCreateSuccess(response.body))
+      })
+      .catch(console.error)
+  }
